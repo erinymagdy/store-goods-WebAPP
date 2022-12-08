729eb0e785a3bef3cb362bb45b53f3f092de4a0f
Eriny Magdy <erinymagdy1@gmail.com>
2022-12-08 15:27:30 +0200

    Edit in startup

--- store’ goods WebAPP/Startup.cs
+++ store’ goods WebAPP/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -26,34 +27,74 @@ namespace store__goods_WebAPP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddHttpContextAccessor();
+            //To avoid the MultiPartBodyLength error
+            services.Configure<FormOptions>(o =>
+            {
+                o.ValueLengthLimit = int.MaxValue;
+                o.MultipartBodyLengthLimit = int.MaxValue;
+                o.MemoryBufferThreshold = int.MaxValue;
+            });
+            //services.AddDbContext<PhysiotherapyContext>(option);
+            //services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddCors(p => p.AddPolicy("AllowAll", c => c.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin().SetIsOriginAllowed(a => true)));
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "store__goods_WebAPP", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PhysiotherapyWebApp", Version = "v1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "store__goods_WebAPP v1"));
+            //}
+
+            //app.UseHttpsRedirection();
+
+            //app.UseRouting();
+
+            //app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "store__goods_WebAPP v1"));
             }
-
-            app.UseHttpsRedirection();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PhysiotherapyWebApp v1"));
+            app.UseCors("AllowAll");
+            app.UseStaticFiles();
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            //app.UseSpa(spa =>
+            //{
+            //    if (env.IsDevelopment())
+            //        spa.Options.SourcePath = "ClientApp";
+            //    else
+            //        spa.Options.SourcePath = "ClientApp/dist/Physiotherapy";
+
+            //    if (env.IsDevelopment())
+            //    {
+            //        spa.UseAngularCliServer(npmScript: "start");
+            //    }
+
+            //});
         }
     }
 }
